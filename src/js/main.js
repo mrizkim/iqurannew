5390321afdcdc2f3e50ccbc0ed9c2f8cb30f1bd4
mrizkim <mmuhamadrizki@gmail.com>
2023-02-28 22:51:31 +0700

    add ststus active navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 // index.html
-function showSurah() {
-  $.getJSON("https://quran-api-id.vercel.app/surahs", function (data) {
-    $.each(data, function (i, data) {
-      const elemenList = ` <div class="item nama_s"  no-surah="${data.number}" nama-surah="${data.name}"><div class="no-surat">
+const indexPage = () => {
+  function showSurah() {
+    $.getJSON("https://quran-api-id.vercel.app/surahs", function (data) {
+      $.each(data, function (i, data) {
+        const elemenList = ` <div class="item nama_s"  no-surah="${data.number}" nama-surah="${data.name}"><div class="no-surat">
       <p class="number" >${data.number}</p>
       </div>
       <div class="nama-surat ">
@@ -14,31 +15,31 @@ function showSurah() {
       </div>
       </div>`;
 
-      $("#list-item").append(elemenList);
+        $("#list-item").append(elemenList);
+      });
+
+      $(".nama_s").click(function () {
+        document.location.href = `surah.html?${$(this).attr("no-surah")}`;
+      });
     });
-
-    $(".nama_s").click(function () {
-      document.location.href = `surah.html?${$(this).attr("no-surah")}`;
-    });
-  });
-}
-showSurah();
-
-$("#searching").on("keyup", function () {
-  var value = $("#searching").val().toLowerCase();
-  let content = "";
-  var myExp = new RegExp(value, "i");
-
-  if (value == 0) {
-    showSurah();
-    return;
   }
-  $.getJSON("https://quran-api-id.vercel.app/surahs", function (data) {
-    $.each(data, function (i, data) {
-      let replaceData = data.name.replace("-", " ");
-      let replaceDataNonS = replaceData.replaceAll("'", "");
-      if (replaceDataNonS.search(myExp) != -1) {
-        content += ` <div class="item nama_s"  no-surah="${data.number}" nama-surah="${data.name}"><div class="no-surat">
+  showSurah();
+
+  $("#searching").on("keyup", function () {
+    var value = $("#searching").val().toLowerCase();
+    let content = "";
+    var myExp = new RegExp(value, "i");
+
+    if (value == 0) {
+      showSurah();
+      return;
+    }
+    $.getJSON("https://quran-api-id.vercel.app/surahs", function (data) {
+      $.each(data, function (i, data) {
+        let replaceData = data.name.replace("-", " ");
+        let replaceDataNonS = replaceData.replaceAll("'", "");
+        if (replaceDataNonS.search(myExp) != -1) {
+          content += ` <div class="item nama_s"  no-surah="${data.number}" nama-surah="${data.name}"><div class="no-surat">
         <p class="number" >${data.number}</p>
         </div>
         <div class="nama-surat ">
@@ -49,15 +50,16 @@ $("#searching").on("keyup", function () {
         <p>${data.numberOfAyahs} ayat</p>
         </div>
         </div>`;
-      }
-    });
+        }
+      });
 
-    $("#list-item").html(content);
-    $(".nama_s").click(function () {
-      document.location.href = `surah.html?${$(this).attr("no-surah")}`;
+      $("#list-item").html(content);
+      $(".nama_s").click(function () {
+        document.location.href = `surah.html?${$(this).attr("no-surah")}`;
+      });
     });
   });
-});
+};
 
 // surah.html
 
@@ -108,11 +110,11 @@ const surahPage = () => {
 };
 
 // doa.html
-
-$.getJSON("https://islamic-api-zhirrr.vercel.app/api/doaharian", function (data) {
-  let doa = data.data;
-  $.each(doa, function (i, data) {
-    const elemenList = `<div class="item">
+const doaPage = () => {
+  $.getJSON("https://islamic-api-zhirrr.vercel.app/api/doaharian", function (data) {
+    let doa = data.data;
+    $.each(doa, function (i, data) {
+      const elemenList = `<div class="item">
     <div class="no-surat">
     <p class="number">${i + 1}</p>
     </div>
@@ -124,36 +126,37 @@ $.getJSON("https://islamic-api-zhirrr.vercel.app/api/doaharian", function (data)
     </div>
     </div>`;
 
-    $("#item-doa").append(elemenList);
-    i++;
+      $("#item-doa").append(elemenList);
+      i++;
+    });
   });
-});
+};
 
 // jadwalSholat.html
-
-$.getJSON("https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/kota.json", function (data) {
-  $.each(data, function (i, data) {
-    const elemenList = `
-    <option value="${data}">${data}</option>`;
-    $("#dropdown-jadwal").append(elemenList);
-  });
-});
-
-$("#dropdown-jadwal").on("change", function () {
-  const d = new Date();
-  const month = ("0" + (d.getMonth() + 1)).slice(-2);
-  const year = d.getFullYear();
-  const day = ("0" + (d.getDate() + 1)).slice(-2);
-  const value = $("#dropdown-jadwal option:selected").val();
-  const newValue = `${value}/${year}/${month}`;
-  let content = "";
-
-  const apiSholat = `https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${newValue}.json`;
-  $.getJSON(apiSholat, function (data) {
+const jadwalPage = () => {
+  $.getJSON("https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/kota.json", function (data) {
     $.each(data, function (i, data) {
-      const currentDate = `${year}-${month}-${day}`;
-      if (currentDate === data.tanggal) {
-        content += ` 
+      const elemenList = `
+    <option value="${data}">${data}</option>`;
+      $("#dropdown-jadwal").append(elemenList);
+    });
+  });
+
+  $("#dropdown-jadwal").on("change", function () {
+    const d = new Date();
+    const month = (d.getMonth() + 1).toString().length === 1 ? `0${d.getMonth() + 1}` : d.getMonth() + 1;
+    const year = d.getFullYear();
+    const day = d.getDate().toString().length === 1 ? `0${d.getDate()}` : d.getDate();
+    const value = $("#dropdown-jadwal option:selected").val();
+    const newValue = `${value}/${year}/${month}`;
+    let content = "";
+
+    const apiSholat = `https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${newValue}.json`;
+    $.getJSON(apiSholat, function (data) {
+      $.each(data, function (i, data) {
+        const currentDate = `${year}-${month}-${day}`;
+        if (currentDate === data.tanggal) {
+          content += ` 
 
         <h2>${value} || ${currentDate}</h2>
         <div class="item-sholat">
@@ -212,64 +215,70 @@ $("#dropdown-jadwal").on("change", function () {
         </div>
         <p class="number" >${data.isya}</p>
         </div>`;
-      }
-    });
+        }
+      });
 
-    $("#list-jadwal").html(content);
+      $("#list-jadwal").html(content);
+    });
   });
-});
+};
 
 // NGAJI.HTML
-
-$("#dropdown-ngaji").on("change", function () {
-  const value = $("#dropdown-ngaji option:selected").val();
-  if (value === "hijaiyah") {
-    var x = document.getElementById("content-hijaiyah");
-    var y = document.getElementById("list-nabi");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+const ngajiPage = () => {
+  $("#dropdown-ngaji").on("change", function () {
+    const value = $("#dropdown-ngaji option:selected").val();
+    if (value === "hijaiyah") {
+      var x = document.getElementById("content-hijaiyah");
+      var y = document.getElementById("list-nabi");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+        y.style.display = "none";
+      }
+    } else if (value === "nabi") {
+      var x = document.getElementById("content-hijaiyah");
+      var y = document.getElementById("list-nabi");
+      if (y.style.display === "block") {
+        y.style.display = "none";
+      } else {
+        y.style.display = "block";
+        x.style.display = "none";
+      }
     } else {
-      x.style.display = "block";
+      var x = document.getElementById("content-hijaiyah");
+      var y = document.getElementById("list-nabi");
       y.style.display = "none";
-    }
-  } else {
-    var x = document.getElementById("content-hijaiyah");
-    var y = document.getElementById("list-nabi");
-    if (y.style.display === "block") {
-      y.style.display = "none";
-    } else {
-      y.style.display = "block";
       x.style.display = "none";
     }
-  }
-});
+  });
 
-// $("#hijaiyah").click(function () {
-//   var x = document.getElementById("content-hijaiyah");
-//   var y = document.getElementById("list-nabi");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//     y.style.display = "none";
-//   }
-// });
+  // $("#hijaiyah").click(function () {
+  //   var x = document.getElementById("content-hijaiyah");
+  //   var y = document.getElementById("list-nabi");
+  //   if (x.style.display === "block") {
+  //     x.style.display = "none";
+  //   } else {
+  //     x.style.display = "block";
+  //     y.style.display = "none";
+  //   }
+  // });
 
-// $("#kisah-nabi").click(function () {
-//   var x = document.getElementById("content-hijaiyah");
-//   var y = document.getElementById("list-nabi");
-//   if (y.style.display === "block") {
-//     y.style.display = "none";
-//   } else {
-//     y.style.display = "block";
-//     x.style.display = "none";
-//   }
-// });
+  // $("#kisah-nabi").click(function () {
+  //   var x = document.getElementById("content-hijaiyah");
+  //   var y = document.getElementById("list-nabi");
+  //   if (y.style.display === "block") {
+  //     y.style.display = "none";
+  //   } else {
+  //     y.style.display = "block";
+  //     x.style.display = "none";
+  //   }
+  // });
 
-// KISAH NABI
-$.getJSON("https://islamic-api-zhirrr.vercel.app/api/kisahnabi", function (data) {
-  $.each(data, function (i, data) {
-    const elemenList = `<div class="content hideContent">
+  // KISAH NABI
+  $.getJSON("https://islamic-api-zhirrr.vercel.app/api/kisahnabi", function (data) {
+    $.each(data, function (i, data) {
+      const elemenList = `<div class="content hideContent">
   <h1>${data.name}</h1>
   <p class="kisah">${data.description}</p>
 </div>
@@ -278,40 +287,44 @@ $.getJSON("https://islamic-api-zhirrr.vercel.app/api/kisahnabi", function (data)
         <p>Show more</p>
     </div>`;
 
-    $("#list-nabi").append(elemenList);
-  });
+      $("#list-nabi").append(elemenList);
+    });
 
-  $("#show-more p").on("click", function () {
-    var showMore = $(this);
-    var content = showMore.parent().prev("div.content");
-    var linkText = showMore.text().toUpperCase();
-    if (linkText === "SHOW MORE") {
-      linkText = "Show less";
-      content.switchClass("hideContent", "showContent", 100);
-    } else {
-      linkText = "Show more";
-      content.switchClass("showContent", "hideContent", 100);
-    }
+    $("#show-more p").on("click", function () {
+      var showMore = $(this);
+      var content = showMore.parent().prev("div.content");
+      var linkText = showMore.text().toUpperCase();
+      if (linkText === "SHOW MORE") {
+        linkText = "Show less";
+        content.switchClass("hideContent", "showContent", 100);
+      } else {
+        linkText = "Show more";
+        content.switchClass("showContent", "hideContent", 100);
+      }
 
-    showMore.text(linkText);
+      showMore.text(linkText);
+    });
   });
-});
+};
 
 // KALENDER
-const d = new Date();
-const month = ("0" + (d.getMonth() + 1)).slice(-2);
-const year = d.getFullYear();
-const day = ("0" + (d.getDate() + 1)).slice(-2);
-const value = $("#dropdown-jadwal option:selected").val();
-const newValue = `bogor/${year}/${month}`;
-let content = "";
+const kalenderPage = () => {
+  const d = new Date();
+  const month = ("0" + (d.getMonth() + 1)).slice(-2);
+  const year = d.getFullYear();
+  const day = d.getDate().toString().length === 1 ? `0${d.getDate()}` : d.getDate();
+  // const day = ("0" + (d.getDate() + 1)).slice(-2);
+  // const value = $("#dropdown-jadwal option:selected").val();
+  const newValue = `bogor/${year}/${month}`;
+  console.log(day);
+  let content = "";
 
-const apiSholat = `https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${newValue}.json`;
-$.getJSON(apiSholat, function (data) {
-  $.each(data, function (i, data) {
-    content += ` 
+  const apiSholat = `https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${newValue}.json`;
+  $.getJSON(apiSholat, function (data) {
+    $.each(data, function (i, data) {
+      content += ` 
       <tr>
-        <td style="width: 25rem" class="mountDate">${data.tanggal}</td>
+        <td style="width: 25rem" id="mountDate">${data.tanggal}</td>
         <td class="dateTime">${data.imsyak}</td>
         <td class="dateTime">${data.shubuh}</td>
         <td class="dateTime">${data.dhuha}</td>
@@ -322,17 +335,18 @@ $.getJSON(apiSholat, function (data) {
       </tr>
 
         `;
+    });
+
+    $("#kalender").html(content);
+
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(function (position) {
+    //     var lat = position.coords.latitude;
+    //     var lng = position.coords.longitude;
+    //     console.log("Latitude: " + lat + ", Longitude: " + lng);
+    //   });
+    // } else {
+    //   console.log("Geolocation is not supported by this browser.");
+    // }
   });
-
-  $("#kalender").html(content);
-
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(function (position) {
-  //     var lat = position.coords.latitude;
-  //     var lng = position.coords.longitude;
-  //     console.log("Latitude: " + lat + ", Longitude: " + lng);
-  //   });
-  // } else {
-  //   console.log("Geolocation is not supported by this browser.");
-  // }
-});
+};
